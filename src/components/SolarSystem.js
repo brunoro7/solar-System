3cd9ebc33439aae5b2a0bddfb407f8daae4aa6c8
brunoro7 <bruno.ro7c@gmail.com>
2022-02-18 23:49:07 -0300

    entregando requisito 2.

---
+++ src/components/SolarSystem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div className="box-solarSytem" data-testid="solar-system">
+        <p>aqui estara o title</p>
+      </div>
+    );
+  }
+}
+
+export default SolarSystem;
